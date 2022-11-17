acedffdadc29c45a7be08cc7963b11dae6a8c10b
samy19911991 <sajobome1991@gmail.com>
2022-11-16 21:49:01 -0500

    wwwww

--- TrialApp/WindowsFormsApp1/Form1.Designer.cs
+++ TrialApp/WindowsFormsApp1/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +43,21 @@ namespace WindowsFormsApp1
             this.label1.TabIndex = 0;
             this.label1.Text = "version 7.0";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(496, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 108);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +69,7 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
