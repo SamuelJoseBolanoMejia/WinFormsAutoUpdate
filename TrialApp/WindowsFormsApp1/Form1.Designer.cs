eecbdc47c790f6f2266f52705ac97412239dbfef
samy19911991 <sajobome1991@gmail.com>
2022-11-17 00:22:27 -0500

    ggg

--- TrialApp/WindowsFormsApp1/Form1.Designer.cs
+++ TrialApp/WindowsFormsApp1/Form1.Designer.cs
@@ -34,6 +34,8 @@ namespace WindowsFormsApp1
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
             this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,11 +57,30 @@ namespace WindowsFormsApp1
             this.domainUpDown1.TabIndex = 1;
             this.domainUpDown1.Text = "domainUpDown1";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(269, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(502, 322);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.domainUpDown1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -73,6 +94,8 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Diagnostics.PerformanceCounter performanceCounter1;
         private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
