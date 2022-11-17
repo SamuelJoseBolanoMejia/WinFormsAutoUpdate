206baa100308971d76915faa0678f24643f10a70
samy19911991 <sajobome1991@gmail.com>
2022-11-16 21:45:11 -0500

    xxx

--- TrialApp/WindowsFormsApp1/Form1.Designer.cs
+++ TrialApp/WindowsFormsApp1/Form1.Designer.cs
@@ -36,11 +36,11 @@ namespace WindowsFormsApp1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(215, 132);
+            this.label1.Location = new System.Drawing.Point(141, 141);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(432, 108);
+            this.label1.Size = new System.Drawing.Size(512, 108);
             this.label1.TabIndex = 0;
-            this.label1.Text = "version 6";
+            this.label1.Text = "version 7.0";
             // 
             // Form1
             // 
